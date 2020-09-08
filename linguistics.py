12048d1f8464e66236d432a3993cdcf676d24ee5
Kalen Chang <1614171+kalenchang@users.noreply.github.com>
2020-09-08 16:27:21 -0700

    create linguistics

---
+++ linguistics.py
@@ -0,0 +1,20 @@
+class Linguistics:
+
+    def __init__(self):
+        pass
+
+
+class Consonant:
+
+    def __init__(self, place, manner, voicing):
+        self.place = place
+        self.manner = manner
+        self.voicing = voicing
+
+
+class Vowel:
+
+    def __init__(self, height, backness, tenseness):
+        self.height = height
+        self.backness = backness
+        self.tenseness = tenseness
