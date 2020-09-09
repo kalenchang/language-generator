b537f39cccbb03fdece141dc38b51a2120834d42
Kalen Chang <1614171+kalenchang@users.noreply.github.com>
2020-09-08 17:01:03 -0700

    add phonology

--- linguistics.py
+++ linguistics.py
@@ -1,20 +1,37 @@
 class Linguistics:
 
     def __init__(self):
-        pass
+        self.phonology = {}
+        self.createPhonology()
+
+    def createPhonology(self):
+        self.phonology["p"] = Consonant("p", "bilabial", "plosive", "voiceless")
+        self.phonology["t"] = Consonant("t", "alveolar", "plosive", "voiceless")
+        self.phonology["k"] = Consonant("k", "velar", "plosive", "voiceless")
+        self.phonology["b"] = Consonant("b", "bilabial", "plosive", "voiced")
+        self.phonology["d"] = Consonant("d", "alveolar", "plosive", "voiced")
+        self.phonology["g"] = Consonant("g", "velar", "plosive", "voiced")
 
 
 class Consonant:
 
-    def __init__(self, place, manner, voicing):
+    def __init__(self, symbol, place, manner, voicing):
+        self.symbol = symbol
         self.place = place
         self.manner = manner
         self.voicing = voicing
 
+    def __repr__(self):
+        return self.symbol + ': ' + self.place + ', ' + self.manner + ', ' + self.voicing
+
+    def __str__(self):
+        return self.symbol
+
 
 class Vowel:
 
-    def __init__(self, height, backness, tenseness):
+    def __init__(self, symbol, height, backness, tenseness):
+        self.symbol = symbol
         self.height = height
         self.backness = backness
         self.tenseness = tenseness
